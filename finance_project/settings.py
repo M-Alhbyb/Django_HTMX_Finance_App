e72a41cbf22cafe16a1825265f8b6a67ec30a88e
pasha216 <mohammedalhbyb@gmail.com>
2025-10-11 20:48:12 +0200

    False debug in settings.py

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hfjjz86s+pz6y4p=&mxz)dis&l$+mt2)6r$ho@))^5))f#$whp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'Pasha-Finance-App.onrender.com']
 
